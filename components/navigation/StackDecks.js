f8bdbe84aaa7117e9f13cc0ec1f25c72a8d80997
faycel <debian2007.faical@gmail.com>
2020-05-06 22:56:19 +0100

    added animation to decks stack

--- components/navigation/StackDecks.js
+++ components/navigation/StackDecks.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { createStackNavigator } from "@react-navigation/stack";
+import { createStackNavigator, TransitionPresets,TransitionSpecs,CardStyleInterpolators } from "@react-navigation/stack";
 import AddCard from "../AddCard";
 import Quiz from "../Quiz";
 import List from "../List";
@@ -9,7 +9,19 @@ import Deck from "../Deck";
 const StackDecks = createStackNavigator();
 
 export const StackDecksComponent = () => (
-  <StackDecks.Navigator>
+  <StackDecks.Navigator screenOptions={{
+    gestureEnabled:true,
+    gestureDirection:"horizontal",
+    ...TransitionPresets.SlideFromRightIOS,
+    // cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS
+    // TransitionSpecs:{
+    //   open:animationConfig,
+    //   close:animationCloseConfig,
+    // }
+  }}
+  headerMode="float"
+  animation="fade"
+  >
     <StackDecks.Screen
       name="DecksList"
       component={List}
@@ -59,3 +71,25 @@ export const StackDecksComponent = () => (
     />
   </StackDecks.Navigator>
 );
+
+// Animation config
+// const animationCloseConfig = {
+//   animation: 'timing',
+//   config:{
+//     duration: 500,
+//     easing: Easing.linear,
+//   }
+// }
+
+// const animationConfig = {
+//   animation: 'spring',
+//   config: {
+//     stiffness: 1000,
+//     damping: 50,
+//     mass: 3,
+//     overshootClamping: false,
+//     restDisplacementThreshold: 0.01,
+//     restSpeedThreshold: 0.01,
+//   },
+// };
+
